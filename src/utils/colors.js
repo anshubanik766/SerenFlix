2519da30457b7680e457af288f89cbc6847ba6e6
Anshu Banik <158592119+anshubanik766@users.noreply.github.com>
2026-01-20 18:12:51 +0530

    Add color constants for status indicators

---
+++ src/utils/colors.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+    SUCCESS: 0x57F287, // green
+    RUNNING: 0x3498DB, // blue
+    ERROR:   0xED4245, // red
+    WARNING: 0xFEE75C, // yellow
+    INFO:    0x9B59B6  // purple
+};
+
